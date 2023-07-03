25f1252dc674678c9e14e569f85bb67be308dee7
PRP <padhupradheep@gmail.com>
2023-07-03 11:54:10 +0200

    first commit

---
+++ src/neo_battery_checker.cpp
@@ -0,0 +1,51 @@
+#include <rclcpp/rclcpp.hpp>
+#include <neo_msgs2/msg/relay_board_v2.hpp>
+
+class NeoBatteryChecker : public rclcpp::Node
+{
+public:
+  NeoBatteryChecker() : Node("neo_battery_checker")
+  {
+    // Declare parameters
+    this->declare_parameter<double>("battery_threshold", 20.0);
+
+    // Get parameters
+    this->get_parameter("battery_threshold", batteryThreshold_);
+
+    // Subscribe to the "state" topic
+    stateSubscriber_ = this->create_subscription<neo_msgs2::msg::RelayBoardV2>(
+      "state", 10, std::bind(&NeoBatteryChecker::stateCallback, this, std::placeholders::_1));
+  }
+
+private:
+  void stateCallback(const neo_msgs2::msg::RelayBoardV2::SharedPtr msg)
+  {
+    // Convert battery percentage to voltage
+    float batteryPercentage = msg->battery_voltage * 100;
+    float batteryVoltage = msg->battery_voltage;
+
+    RCLCPP_INFO(this->get_logger(), "Battery percentage: %.2f%%", batteryPercentage);
+    RCLCPP_INFO(this->get_logger(), "Battery voltage: %.2fV", batteryVoltage);
+
+    // Check if battery percentage goes below the threshold
+    if (batteryPercentage < batteryThreshold_)
+    {
+      RCLCPP_WARN(this->get_logger(), "Battery percentage is below the user specified threshold: %.2f%%", batteryPercentage);
+      
+      // Execute the sudo halt command to shut down the computer
+      std::system("sudo halt");
+    }
+  }
+
+  double batteryThreshold_;
+  rclcpp::Subscription<neo_msgs2::msg::RelayBoardV2>::SharedPtr stateSubscriber_;
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<NeoBatteryChecker>();
+  RCLCPP_INFO(node->get_logger(), "Starting the battery checker node");
+  rclcpp::spin(node);
+  return 0;
+}
